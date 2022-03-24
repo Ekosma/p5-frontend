098f4c177c0eded78135c133d5f5e1a688755907
Evelyn <evelyn.a.kosma@gmail.com>
2022-03-23 21:22:55 -0500

    planned inital three routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { connect } from "react-redux";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/lessons/:id" component={LessonIndex} />
+          <Route path="/lessons/new" component={LessonForm} />
+        </Switch>
+      </Router>
     </div>
   );
 }
