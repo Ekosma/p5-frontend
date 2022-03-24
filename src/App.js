04b0a0877370e85e17292c5a7d8f8f621037f1d9
Evelyn <evelyn.a.kosma@gmail.com>
2022-03-23 21:46:11 -0500

    added import lines for new component shells

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { connect } from "react-redux";
-import './App.css';
+import Header from "./components/Header";
+import Home from "./components/Home";
+import LessonForm from "./components/LessonForm";
+import LessonIndex from "./components/LessonIndex";
+import Footer from "./components/Footer";
+
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Nav />
+        <Header />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/lessons/:id" component={LessonIndex} />
           <Route path="/lessons/new" component={LessonForm} />
         </Switch>
+        <Footer />
       </Router>
     </div>
   );
