67267c254fd7e6ecd830e42e64e16f9524cdf1aa
Evelyn <evelyn.a.kosma@gmail.com>
2022-03-24 18:19:33 -0500

    added cards component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from "./Header";
 import Footer from "./Footer";
+import Search from "./Search";
+import Cards from "./Cards"
+
 
 export default function Home() {
   return (
     <div>
       <Header />
-      
+      <Search />
+      <Cards />
       <Footer />
     </div>
   )
