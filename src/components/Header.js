02836dd36fe9cbfac1a5b79587a11b020634cf93
Evelyn <evelyn.a.kosma@gmail.com>
2022-03-24 18:07:14 -0500

    added links to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function header() {
   return (
     <div>
-      
+      <Link to="/dashboard">
+        Teacher Gives Teachers
+      </Link>
+      <Link to="/lessons/new">
+        Upload Lesson
+      </Link>
     </div>
   )
 }
