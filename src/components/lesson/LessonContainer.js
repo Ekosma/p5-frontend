f529507216145f1a0f92066bb95d8fdf98f83149
Evelyn <evelyn.a.kosma@gmail.com>
2022-04-04 18:50:28 -0500

    attempted to add fake data to then use to create base lesson page

--- src/components/lesson/LessonContainer.js
+++ src/components/lesson/LessonContainer.js
@@ -15,3 +15,4 @@ export default function Lesson() {
   </div>
   )
 }
+
--- src/components/lessons/LessonContainer.js
+++ src/components/lessons/LessonContainer.js
@@ -8,3 +8,7 @@ export default function LessonContainer() {
     </div>
   )
 }
+
+const LESSONS = [
+  {name: 'FUNctions', description: "Functions that are fun", grade: "fifth, sixth, and seventh", content: "THIS IS SUPPOSED TO BE THE PDF SPOT BUT I THINK I MAY NEED TO REDO MY DATABASE D:"}
+]
