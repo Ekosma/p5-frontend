8c24f4be2c907a08c580b5581a7f396ddefd810a
Evelyn <evelyn.a.kosma@gmail.com>
2022-04-05 14:10:40 -0500

    working with checkboxes to sort through data

--- src/components/lessons/Search.js
+++ src/components/lessons/Search.js
@@ -4,13 +4,13 @@ import { useState } from "react";
 // This holds a list of some fiction people
 // Some  have the same name but different age and id
 const LESSONS = [
-  { id: 1, name: 'Quadratics', grade: 'kindergarten', subject:'Mathematics'},
-  { id: 2, name: 'Algebra Review', grade: 'first', subject:'Mathematics'},
-  { id: 3, name: 'Linear Fun', grade: '9th and 11th', subject:'Mathematics'},
-  { id: 4, name: 'English Essay Prompts', grade: '9th, 10th, and 11th', subject:'English' },
-  { id: 5, name: '11th History Exam', grade: '11th', subject:'History'},
-  { id: 6, name: '10th English Quiz', grade: '10th', subject:'English' },
-  { id: 7, name: 'Geometry CrossWord', grade: '10th', subject:'Mathematics'},
+  { id: 1, name: 'Quadratics', grade: 'kindergarten', subject:'math'},
+  { id: 2, name: 'Algebra Review', grade: 'first', subject:'english'},
+  { id: 3, name: 'Linear Fun', grade: 'first', subject:'math'},
+  { id: 4, name: 'English Essay Prompts', grade: 'sixth', subject:'science' },
+  { id: 5, name: '11th History Exam', grade: 'twelfth', subject:'elective'},
+  { id: 6, name: '10th English Quiz', grade: 'fourth', subject:'fineArt' },
+  { id: 7, name: 'Geometry CrossWord', grade: 'fifth', subject:'history'},
 ];
 
 
@@ -39,22 +39,25 @@ function Search(){
     math: false,
     science: false,
     history: false,
-    fineArts: false,
+    fineArt: false,
     athletics: false,
     elective: false,
   });
 
   const onGradeChange = (e) => {
     setGrade({...grade, [e.target.value]: e.target.checked})
+    setLessons()
   }
 
   const onSubjectChange = (e) => {
     setSubject({...subject, [e.target.value]: e.target.checked})
+    setLessons()
   }
 
   return (
     <>
       <div>
+        <h2>Select Grade Level</h2>
         <label>
           <input 
             type="checkbox"
@@ -74,16 +77,212 @@ function Search(){
           />
           first
         </label>
-        <ul>
-          {lessons.filter(x => grade[x.grade]).map((lesson) => (
-            <li key={lesson.id}>{lesson.name}{lesson.grade}</li>
-          ))}
-        </ul>
 
+        <label>
+          <input 
+            type="checkbox"
+            value="second"
+            name="grade"
+            onChange={onGradeChange}
+          />
+          second
+        </label>
+
+        <label>
+          <input 
+            type="checkbox"
+            value="third"
+            name="grade"
+            onChange={onGradeChange}
+          />
+          third
+        </label>
+
+        <label>
+          <input 
+            type="checkbox"
+            value="fourth"
+            name="grade"
+            onChange={onGradeChange}
+          />
+          fourth
+        </label>
+
+        <label>
+          <input 
+            type="checkbox"
+            value="fifth"
+            name="grade"
+            onChange={onGradeChange}
+          />
+          fifth
+        </label>
+
+        <label>
+          <input 
+            type="checkbox"
+            value="sixth"
+            name="grade"
+            onChange={onGradeChange}
+          />
+          sixth
+        </label>
+
+        <label>
+          <input 
+            type="checkbox"
+            value="seventh"
+            name="grade"
+            onChange={onGradeChange}
+          />
+          seventh
+        </label>
+
+        <label>
+          <input 
+            type="checkbox"
+            value="eighth"
+            name="grade"
+            onChange={onGradeChange}
+          />
+          eighth
+        </label>
+
+        <label>
+          <input 
+            type="checkbox"
+            value="ninth"
+            name="grade"
+            onChange={onGradeChange}
+          />
+          ninth
+        </label>
+
+        <label>
+          <input 
+            type="checkbox"
+            value="tenth"
+            name="grade"
+            onChange={onGradeChange}
+          />
+          tenth
+        </label>
+
+        <label>
+          <input 
+            type="checkbox"
+            value="eleventh"
+            name="grade"
+            onChange={onGradeChange}
+          />
+          eleventh
+        </label>
+
+        <label>
+          <input 
+            type="checkbox"
+            value="twelfth"
+            name="grade"
+            onChange={onGradeChange}
+          />
+          twelfth
+        </label>
       </div>
+      <div>
+        <h2>Select Class Type Level</h2>
+
+        <label>
+          <input 
+            type="checkbox"
+            value="english"
+            name="subject"
+            onChange={onSubjectChange}
+          />
+          english
+        </label>
+
+        <label>
+          <input 
+            type="checkbox"
+            value="science"
+            name="subject"
+            onChange={onSubjectChange}
+          />
+          science
+        </label>
+
+        <label>
+          <input 
+            type="checkbox"
+            value="math"
+            name="subject"
+            onChange={onSubjectChange}
+          />
+          math
+        </label>
+
+        <label>
+          <input 
+            type="checkbox"
+            value="history"
+            name="subject"
+            onChange={onSubjectChange}
+          />
+          history
+        </label>
+
+        <label>
+          <input 
+            type="checkbox"
+            value="fineArt"
+            name="subject"
+            onChange={onSubjectChange}
+          />
+          fineArt
+        </label>
+
+        <label>
+          <input 
+            type="checkbox"
+            value="athletics"
+            name="subject"
+            onChange={onSubjectChange}
+          />
+          athletics
+        </label>
+
+        <label>
+          <input 
+            type="checkbox"
+            value="elective"
+            name="subject"
+            onChange={onSubjectChange}
+          />
+          elective
+        </label>
+      </div>
+
+      <ul>
+        {
+
+        }
+      </ul>
+      
+
+      {/*<ul>
+          {lessons.filter(x => subject[x.subject]).filter(y => grade[y.grade]).map((lesson) => (
+            <li key={lesson.id}>
+            Name: {lesson.name}<br />
+            Grade: {lesson.grade}<br />
+            Subject: {lesson.subject}
+          </li>
+          ))}
+        </ul>*/}
     </>
   );
 }
+
+
 
 
 
