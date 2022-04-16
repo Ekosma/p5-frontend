40ee428d3d6c373176309a8412a7e6fe45c905bf
Evelyn <evelyn.a.kosma@gmail.com>
2022-04-16 14:03:33 -0500

    added name and description feild, created object to send to back end with data

--- src/components/lessonForm/LessonFormContainer.js
+++ src/components/lessonForm/LessonFormContainer.js
@@ -8,7 +8,7 @@ import { Form, Button } from 'react-bootstrap';
 class LessonFormContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = {grade: 'pick grade', subject: 'pick subject'};
+    this.state = {grade: 'no grade specified', subject: 'no subject specified', name: 'title', description: 'description'};
   };
 
   gradeChoice = (e) => {
@@ -27,11 +27,34 @@ class LessonFormContainer extends Component {
     })
   }
 
+  nameChoice = (e) => {
+    e.preventDefault();
+    console.log(e.target.value)
+    this.setState({
+      name: e.target.value
+    })
+  }
+
+  descriptionChoice = (e) => {
+    e.preventDefault();
+    console.log(e.target.value)
+    this.setState({
+      description: e.target.value
+    })
+  }
+
   handleSubmit = (e) => {
     e.preventDefault()
     console.log("handleSubmit")
-    console.log(this.state.grade)
-    this.props.fetchLessons(this.state.grade)
+    console.log(e)
+    const lesson = {
+      name: this.state.name,
+      description: this.state.description,
+      grade: this.state.grade,
+      subject: this.state.subject
+    }
+    console.log(lesson)
+    this.props.fetchLessons(lesson)
     console.log("handleSubmit")
     this.setState({
       
@@ -44,9 +67,13 @@ class LessonFormContainer extends Component {
 
     const listGrade = <DropdownItem list={grades} choice={this.state.grade} onClick={this.gradeChoice} />
     const listSubject = <DropdownItem list={subjects} choice={this.state.subject} onClick={this.subjectChoice} />
+    const name = <textarea id="name" rows="1" onChange={this.nameChoice} />
+    const description = <textarea id="description" rows="5" onChange={this.descriptionChoice} />
     return (
       <Form onSubmit={this.handleSubmit}>
         <h1>LESSON FORM PAGE</h1>
+        {name}
+        {description}
         {listGrade}
         {listSubject}
         <Button variant="primary" type="submit">Submit</Button>
