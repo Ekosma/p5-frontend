3225e31ece7e629d812d48df729e04d4c1210dea
Evelyn <evelyn.a.kosma@gmail.com>
2022-04-16 17:03:19 -0500

    added filter for index

--- src/components/lessons/LessonContainer.js
+++ src/components/lessons/LessonContainer.js
@@ -6,23 +6,49 @@ import Card from './Cards'
 
  class LessonContainer extends Component {
 
+
+
+  constructor(props) {
+    super(props);
+    this.state = {grade: 'grade', subject: 'subject'};
+  };
+
   componentDidMount() {
     this.props.indexLessons()
   }
-  
+
+  gradeChoice = (e) => {
+    e.preventDefault();
+    //console.log(e.target.innerHTML)
+    this.setState({
+      grade: e.target.innerHTML
+    })
+  }
+
+  subjectChoice = (e) => {
+    e.preventDefault();
+    //console.log(e.target.innerHTML)
+    this.setState({
+      subject: e.target.innerHTML
+    })
+  }
 
 
   render() {
-    const listLessons = <Card data={this.props.lessons} />
+    const filteredLessons = this.props.lessons
+      .filter(lesson => this.state.grade === 'grade' ? true : lesson.grade === this.state.grade)
+      .filter(lesson => this.state.subject === 'subject' ? true : lesson.subject === this.state.subject)
     console.log(this.props.lessons)
+    console.log(this.state.grade)
+    console.log(filteredLessons)
+    const listLessons = <Card data={filteredLessons} />
+    //console.log(this.props.lessons)
     return ( 
       <div>
+        <Search grade={this.state.grade} subject={this.state.subject} gradeChoice={this.gradeChoice} subjectChoice={this.subjectChoice} />
         {listLessons}
-        <Search />
-        
       </div>
     )
-    
   }
 }
 
--- src/components/lessons/Search.jsx
+++ src/components/lessons/Search.jsx
@@ -3,33 +3,13 @@ import DropdownItem from '../dropdownItem';
 import "./Search.css"
 
 export default class Search extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {grade: 'pick grade', subject: 'pick subject'};
-  };
-
-  gradeChoice = (e) => {
-    e.preventDefault();
-    //console.log(e.target.innerHTML)
-    this.setState({
-      grade: e.target.innerHTML
-    })
-  }
-
-  subjectChoice = (e) => {
-    e.preventDefault();
-    //console.log(e.target.innerHTML)
-    this.setState({
-      subject: e.target.innerHTML
-    })
-  }
-
+  
   render() {
     const grades = ["Kindergarten", "First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh", "Eighth", "Ninth", "Tenth", "Eleventh", "Twelfth" ] 
     const subjects = ["English", "History", "Mathematics", "Science", "Fine Arts", "Athletics", "Elective"]
 
-    const listGrade = <DropdownItem list={grades} choice={this.state.grade} onClick={this.gradeChoice} />
-    const listSubject = <DropdownItem list={subjects} choice={this.state.subject} onClick={this.subjectChoice} />
+    const listGrade = <DropdownItem list={grades} choice={this.props.grade} onClick={this.props.gradeChoice} />
+    const listSubject = <DropdownItem list={subjects} choice={this.props.subject} onClick={this.props.subjectChoice} />
     return (
     <div id="search">
       test test {listGrade} test test {listSubject}
